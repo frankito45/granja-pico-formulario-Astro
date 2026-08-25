c3c72dffbdc913aaf5e36038bf9f47309f8c29f5
franco <mancillafranco71@gmail.com>
2026-08-25 10:52:46 -0300

    refactory y configuraciones

--- src/pages/api/productos.ts
+++ src/pages/api/productos.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../lib/supabase";
 
+import type { Producto } from "../../models/productos";
 
 
 export const prerender = false
@@ -16,10 +17,59 @@ export const GET:APIRoute = async () => {
                 error: error.message
             },
             {
-                status:504
+                status:500
             }
         )
     }
     
     return Response.json(data)
+}
+
+
+export const POST:APIRoute = async ({request}) => {
+    try{
+
+        const data = await request.json() as Producto;   
+        
+        const {nombre, categoria} = data
+        if (!nombre || !categoria) {
+            return Response.json(
+                {
+                success: false,
+                error: "Faltan datos obligatorios"
+                },
+                {
+                status: 400
+                }
+            );
+        }
+
+        const {error} = await supabase.from('productos').insert(data)
+
+        if(error){
+            return Response.json(
+                {
+                    success:false,
+                    error: error.message
+                },
+
+                {status:504}
+
+            )
+        }
+
+        return Response.json(
+            {success:true}
+        )
+
+    }catch(error){
+        return Response.json(
+                {
+                    success:false,
+                    error: "Error al guardar los datos"
+                },
+                {status:500}
+        )
+    }
+
 }
